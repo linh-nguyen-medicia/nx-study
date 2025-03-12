0363e4c58ae742b953b5153dae7b52af17114f0f
thaoptb <thao@medicia.ai>
2025-03-12 23:36:53 +0700

    feat: create users api

--- apps/my-nest-app/src/app/users/users.service.ts
+++ apps/my-nest-app/src/app/users/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '../models/user';
+import { User } from '../../models/user';
 
 @Injectable()
 export class UsersService {
 
-  public getUsers(): { items: User[], totalRecords: number } {
+  listUsers(): { items: User[], totalRecords: number } {
     const users: User[] = [
       { id: 1, name: 'John Doe', email: 'john.doe@example.com' },
       { id: 2, name: 'Jane Smith', email: 'jane.smith@example.com' }
     ];
+
     return {
       items: users,
       totalRecords: users.length
